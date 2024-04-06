e893c5af6d07b2ea6b3470108eb94ecd5d2ee5fc
James Clover <jamesmclover93@gmail.com>
2024-04-05 20:32:59 -0400

    starter server code for rendering

---
+++ server/index.tsx
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+
+
+
+const app = express();
+
+app.use(express.static('client/dist'));
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
