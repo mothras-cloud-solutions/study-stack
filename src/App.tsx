61bdbe14cd955c10b3d00156d397249d1fdd475b
Eric Yap <ericy2296@gmail.com>
2024-04-08 14:28:59 -0700

    Deck Collection components

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,14 @@
-import React, { useState } from 'react';
-import DeckSelector from './components/inspectCollection/DeckSelector';
-import CardThumbnailContainer from './components/inspectCollection/CardThumbnailContainer';
-
-import collectionData from './components/inspectCollection/collectionData.json';
-import flashcardData from './components/inspectCollection/flashcardData.json';
-
-type DeckType = {
-  collectionId: number;
-  title: string;
-  description: string;
-  subjects: string[];
-};
-
-type FlashcardType = {
-  id: number;
-  term: string;
-  definition: string;
-  confidenceLevel: number;
-  keywords: string;
-  collectionId: number;
-};
-
-
+import React from 'react';
+import Card from './components/Card.tsx';
+import { Interface } from 'readline';
 function App() {
-  const [selectedDeck, setSelectedDeck] = useState<DeckType | null>(null);
 
-  // Create a handler for when a deck is selected
-  const handleDeckSelect = (deck: DeckType) => {
-    setSelectedDeck(deck);
-  };
-
-  // Filter the flashcards for the selected deck
-  const selectedFlashcards = selectedDeck
-    ? flashcardData.filter(fc => fc.collectionId === selectedDeck.collectionId)
-    : [];
 
   return (
     <div>
-      <h1>Study Stack</h1>
-      <DeckSelector decks={collectionData} onDeckSelect={handleDeckSelect} />
-      {selectedDeck && (
-        <CardThumbnailContainer flashcards={selectedFlashcards} />
-      )}
+      <p>Hello Inside of App!</p>
     </div>
-  );
+  )
 }
 
 export default App;
